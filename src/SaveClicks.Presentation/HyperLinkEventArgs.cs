0f5e8cea43cd7752a5646e90fa69b948b6cbf70e
Martin Škuta <martin.skuta@outlook.com>
2016-04-30 14:44:44 +0200

    SC-3 Remove comments and regions

--- src/SaveClicks.Presentation/HyperLinkEventArgs.cs
+++ src/SaveClicks.Presentation/HyperLinkEventArgs.cs
@@ -8,19 +8,12 @@ namespace SaveClicks.Presentation
 {
     public class HyperLinkEventArgs : EventArgs
     {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="HyperLinkEventArgs" /> class.
-        /// </summary>
-        /// <param name="hyperlinkObjectForRaisedEvent">The hyper link object for raised event.</param>
         public HyperLinkEventArgs(object hyperlinkObjectForRaisedEvent)
             : this()
         {
             HyperlinkObjectForRaisedEvent = hyperlinkObjectForRaisedEvent;
         }
 
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="HyperLinkEventArgs" /> class.
-        /// </summary>
         public HyperLinkEventArgs()
         {
         }
--- src/SaveClicks.Presentation/ToastPopup.xaml.cs
+++ src/SaveClicks.Presentation/ToastPopup.xaml.cs
@@ -14,11 +14,71 @@ namespace SaveClicks.Presentation
 {
     public partial class ToastPopup
     {
-        #region Public Methods
+        private readonly string _name = typeof(ToastPopup).Name;
+        private volatile object _lockObject = new object();
 
-        /// <summary>
-        ///     Opens a window and returns without waiting for the newly opened window to close.
-        /// </summary>
+        public ToastPopup(string title, string text)
+            : this(title)
+        {
+            TextBoxShortDescription.Text = text;
+        }
+
+        public ToastPopup(string title, List<Inline> textInlines)
+            : this(title)
+        {
+            TextBoxShortDescription.Inlines.AddRange(textInlines);
+        }
+
+        public ToastPopup(string title, string text, string hyperlinkText, object hyperlinkObjectForRaisedEvent = null)
+            : this(title, text)
+        {
+            HyperlinkObjectForRaisedEvent = hyperlinkObjectForRaisedEvent;
+            SetHyperLinkButton(hyperlinkText);
+        }
+
+        public ToastPopup(string title, List<Inline> textInlines, string hyperlinkText, object hyperlinkObjectForRaisedEvent = null)
+            : this(title)
+        {
+            HyperlinkObjectForRaisedEvent = hyperlinkObjectForRaisedEvent;
+            TextBoxShortDescription.Inlines.AddRange(textInlines);
+            SetHyperLinkButton(hyperlinkText);
+        }
+
+        private ToastPopup(string title)
+        {
+            InitializeComponent();
+            Application.Current.MainWindow.Closing += MainWindowClosing;
+
+            TextBoxTitle.Text = title;
+        }
+
+        public Brush FontColor
+        {
+            get { return TextBoxTitle.Foreground; }
+
+            set
+            {
+                TextBoxTitle.Foreground = value;
+                TextBoxShortDescription.Foreground = value;
+            }
+        }
+
+        public new Brush BorderBrush
+        {
+            get { return BorderBackground.BorderBrush; }
+
+            set { BorderBackground.BorderBrush = value; }
+        }
+
+        public new Brush Background
+        {
+            get { return BorderBackground.Background; }
+
+            set { BorderBackground.Background = value; }
+        }
+
+        public object HyperlinkObjectForRaisedEvent { get; set; }
+
         public new void Show()
         {
             var focusedElement = Keyboard.FocusedElement;
@@ -42,176 +102,25 @@ namespace SaveClicks.Presentation
             }
         }
 
-        #endregion
-
-        #region Members
-
-        private readonly string _name = typeof(ToastPopup).Name;
-        private volatile object _lockObject = new object();
-        private string _title;
-
-        #endregion Members
-
-        #region Constructors
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ToastPopup" /> class.
-        /// </summary>
-        /// <param name="title">The title.</param>
-        /// <param name="text">The text.</param>
-        public ToastPopup(string title, string text)
-            : this(title)
-        {
-            TextBoxShortDescription.Text = text;
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ToastPopup" /> class.
-        /// </summary>
-        /// <param name="title">The title.</param>
-        /// <param name="textInlines"></param>
-        public ToastPopup(string title, List<Inline> textInlines)
-            : this(title)
-        {
-            TextBoxShortDescription.Inlines.AddRange(textInlines);
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ToastPopup" /> class.
-        /// </summary>
-        /// <param name="title">The title.</param>
-        /// <param name="text">The text.</param>
-        /// <param name="hyperlinkText">The hyperlink text.</param>
-        /// <param name="hyperlinkObjectForRaisedEvent">The hyperlink object for raised event.</param>
-        public ToastPopup(string title, string text, string hyperlinkText, object hyperlinkObjectForRaisedEvent = null)
-            : this(title, text)
-        {
-            HyperlinkObjectForRaisedEvent = hyperlinkObjectForRaisedEvent;
-            SetHyperLinkButton(hyperlinkText);
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ToastPopup" /> class.
-        /// </summary>
-        /// <param name="title">The title.</param>
-        /// <param name="textInlines">The text inlines.</param>
-        /// <param name="hyperlinkText">The hyperlink text.</param>
-        /// <param name="hyperlinkObjectForRaisedEvent">The hyperlink object for raised event.</param>
-        public ToastPopup(string title, List<Inline> textInlines, string hyperlinkText, object hyperlinkObjectForRaisedEvent = null)
-            : this(title)
-        {
-            HyperlinkObjectForRaisedEvent = hyperlinkObjectForRaisedEvent;
-            TextBoxShortDescription.Inlines.AddRange(textInlines);
-            SetHyperLinkButton(hyperlinkText);
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ToastPopup" /> class.
-        /// </summary>
-        /// <param name="title">The title.</param>
-        private ToastPopup(string title)
-        {
-            InitializeComponent();
-            Application.Current.MainWindow.Closing += MainWindowClosing;
-
-            TextBoxTitle.Text = title;
-        }
-
-        #endregion Constructors
-
-        #region Public Properties
-
-        /// <summary>
-        ///     Gets or sets the color of the font.
-        /// </summary>
-        /// <value>
-        ///     The color of the font.
-        /// </value>
-        public Brush FontColor
-        {
-            get { return TextBoxTitle.Foreground; }
-
-            set
-            {
-                TextBoxTitle.Foreground = value;
-                TextBoxShortDescription.Foreground = value;
-            }
-        }
-
-        /// <summary>
-        ///     Gets or sets a brush that describes the border background of a control.
-        /// </summary>
-        public new Brush BorderBrush
-        {
-            get { return BorderBackground.BorderBrush; }
-
-            set { BorderBackground.BorderBrush = value; }
-        }
-
-        /// <summary>
-        ///     Gets or sets a brush that describes the background of a control.
-        /// </summary>
-        public new Brush Background
-        {
-            get { return BorderBackground.Background; }
-
-            set { BorderBackground.Background = value; }
-        }
-
-        /// <summary>
-        ///     Gets or sets the hyperlink object for raised event.  This object will be passed back when
-        ///     the control raises the HyperlinkClicked event.
-        /// </summary>
-        /// <value>
-        ///     The hyperlink object for raised event.
-        /// </value>
-        public object HyperlinkObjectForRaisedEvent { get; set; }
-
-        #endregion Public Properties
-
-        #region Events
-
         public event EventHandler<EventArgs> ClosedByUser;
 
         public event EventHandler<HyperLinkEventArgs> HyperlinkClicked;
 
-        #endregion Events
-
-        #region Event Handlers
-
-        /// <summary>
-        ///     Raises the <see cref="E:ClosedByUser" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        protected virtual void OnClosedByUser(EventArgs e)
+        private void OnClosedByUser(EventArgs e)
         {
             ClosedByUser?.Invoke(this, e);
         }
 
-        /// <summary>
-        ///     Raises the <see cref="E:HyperlinkClicked" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        protected virtual void OnHyperlinkClicked(HyperLinkEventArgs e)
+        private void OnHyperlinkClicked(HyperLinkEventArgs e)
         {
             HyperlinkClicked?.Invoke(this, e);
         }
 
-        /// <summary>
-        ///     Buttons the view click.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
         private void ButtonViewClick(object sender, RoutedEventArgs e)
         {
-            OnHyperlinkClicked(new HyperLinkEventArgs {HyperlinkObjectForRaisedEvent = HyperlinkObjectForRaisedEvent});
+            OnHyperlinkClicked(new HyperLinkEventArgs { HyperlinkObjectForRaisedEvent = HyperlinkObjectForRaisedEvent });
         }
 
-        /// <summary>
-        ///     Doubles the animation completed.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void DoubleAnimationCompleted(object sender, EventArgs e)
         {
             if (!IsMouseOver)
@@ -220,22 +129,6 @@ namespace SaveClicks.Presentation
             }
         }
 
-        /// <summary>
-        ///     Images the mouse up.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="MouseButtonEventArgs" /> instance containing the event data.</param>
-        private void ImageMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            OnClosedByUser(new EventArgs());
-            Close();
-        }
-
-        /// <summary>
-        ///     Mains the window closing.
-        /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="System.ComponentModel.CancelEventArgs" /> instance containing the event data.</param>
         private void MainWindowClosing(object sender, CancelEventArgs e)
         {
             foreach (Window window in Application.Current.Windows)
@@ -272,15 +165,6 @@ namespace SaveClicks.Presentation
             AdjustWindows();
         }
 
-        #endregion Event Handlers
-
-        #region Private Methods
-
-        /// <summary>
-        ///     Dow windows exist to the right.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <returns></returns>
         private bool WindowsExistToTheRight(double left)
         {
             var mainWindowRect = WindowHelpers.GetMainWindowRect();
@@ -300,9 +184,6 @@ namespace SaveClicks.Presentation
             return false;
         }
 
-        /// <summary>
-        ///     Adjusts the windows.
-        /// </summary>
         private void AdjustWindows()
         {
             lock (_lockObject)
@@ -337,10 +218,6 @@ namespace SaveClicks.Presentation
             }
         }
 
-        /// <summary>
-        ///     Sets the hyperlink button.
-        /// </summary>
-        /// <param name="hyperlinkText">The hyperlink text.</param>
         private void SetHyperLinkButton(string hyperlinkText)
         {
             if (!string.IsNullOrWhiteSpace(hyperlinkText))
@@ -349,7 +226,5 @@ namespace SaveClicks.Presentation
                 ButtonView.Visibility = Visibility.Visible;
             }
         }
-
-        #endregion Private Methods
     }
 }
